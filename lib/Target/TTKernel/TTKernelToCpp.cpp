b16b2e5fb3c9640398605a86cf90d524bd95f66f
Anoop J S <anoop@polymagelabs.com>
2025-09-25 15:34:48 +0530

    Fix 'copy_dest_values' not in scope error due to missing header file

--- lib/Target/TTKernel/TTKernelToCpp.cpp
+++ lib/Target/TTKernel/TTKernelToCpp.cpp
@@ -54,6 +54,8 @@ public:
                                         /*isStandard=*/false);
       builder->create<emitc::IncludeOp>(loc, "compute_kernel_api/common.h",
                                         /*isStandard=*/false);
+      builder->create<emitc::IncludeOp>(loc, "compute_kernel_api/copy_dest_values.h",
+                                        /*isStandard=*/false);
       builder->create<emitc::IncludeOp>(loc, "compute_kernel_api/matmul.h",
                                         /*isStandard=*/false);
       builder->create<emitc::IncludeOp>(loc, "compute_kernel_api/bcast.h",
